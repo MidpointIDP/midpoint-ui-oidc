b16b0f8f95e8bd5933c903203bb9cd5e7a944606
Terry Ott <Terry.Ott@gmail.com>
2026-04-13 12:20:37 +0000

    May have event listener now

---
+++ src/js/signup.js
@@ -0,0 +1,9 @@
+
+function signupGoogle() {
+    console.log("Google sign up button was clicked");
+
+    // Set up URL for Google auth endpoint including all the CSRF goodness
+}
+
+// Add event listener on sign up button
+document.getElementById('button_google_sign_up').addEventListener('click', signupGoogle);
